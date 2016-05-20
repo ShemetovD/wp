8e26a3ba2fe23dddc65d262703c8df075c459c86
Denis Shemetov <youepidemic@gmail.com>
2016-05-20 16:42:30 +0300

    lost files commit

---
+++ themes/shemetov/index.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @package WordPress
+ * @subpackage Shemetov
+ * @since 1.0
+ */
+
+get_header(); ?>
+
+	<div  class="content-area">
+		<main  class="site-main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+			// Start the loop.
+			while ( have_posts() ) : the_post(); ?>
+				
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				
+				
+				<?php
+					the_title();	
+                	//the_content();
+                	the_excerpt();
+                ?>
+
+                <small><?php the_time('F jS, Y') ?> by <?php the_author() ?></small>
+
+			<?php endwhile;
+
+			// Previous/next page navigation.
+			//next_posts_link();
+			//previous_posts_link();
+			the_posts_pagination();
+
+		else :?>
+			<p><?php _e( 'Sorry, no posts matched your criteria' ); ?></p>
+		<?endif;
+		?>
+
+		</main>
+	</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
